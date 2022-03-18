958fd93923f62968620bb78bf6fc80baed4f6be9
Derrick Maurais <derrickmaurais1@gmail.com>
2022-03-17 23:11:00 -0300

    layed ground work. starting into the Javascript

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # roshambo2
+
+I had to cut my losses and start over. My first attempt had all my code tangled. unorganized mess. All the struggling helped me see a bunch of stuff that didnt work. This second try i feel like im laying a much better foundation. Supprisingly it doesn't feel too bad. I like how this second attempt is turning out much better. Here is to the first commit/push of a fresh start.
---
+++ rosh.js
@@ -0,0 +1,25 @@
+// grab/ make HTML elements here. 
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
+
+//defining other game objects.
+let winner = "";
+let gameOver = true;
+
+//game functions.
+
+function resetGame(){};
+
+if (gameOver == false) {
+    alert("game over");
+    alert(winner);
+    resetGame();
+}
+// event listeners.
+rock.addEventListener("click",);
+paper.addEventListener("click",);
+scissors.addEventListener("click",);
+
+
+
